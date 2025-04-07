a67b78ef24040bcddfe2e7dd7ac77219a47181a8
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-07 23:25:09 +0300

    added

---
+++ src/components/todo.js
@@ -0,0 +1,15 @@
+import React,{useState} from "react";
+import '../styles/todo.css'
+
+function Todolist (){
+    const [task, setTask] = useState([]);
+    const [newTask, setNewTask] = useState('')
+
+    const addTask = () => {
+        if (newTask !== '') {
+
+        }
+    };
+}
+
+export default Todolist;
