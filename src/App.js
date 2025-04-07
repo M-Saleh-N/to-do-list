282b888385211ffda5b023a69728b88232dc4ea0
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-07 23:22:40 +0300

    linked it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Todolist from './components/todo';
 
 function App() {
   return (
     <div className="App">
-
+      <Todolist/>
     </div>
   );
 }
